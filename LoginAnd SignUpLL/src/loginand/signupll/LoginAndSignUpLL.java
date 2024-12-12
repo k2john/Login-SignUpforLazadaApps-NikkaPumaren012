48cf9316a54166f5c64e825d1697c4881371c730
k2john <johnmenguis@gmail.com>
2024-12-12 09:02:31 +0800

    Add files via upload

---
+++ LoginAnd SignUpLL/src/loginand/signupll/LoginAndSignUpLL.java
@@ -0,0 +1,17 @@
+package loginand.signupll;
+
+ 
+
+
+ 
+public class LoginAndSignUpLL {
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
